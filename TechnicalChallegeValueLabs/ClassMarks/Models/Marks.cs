928bb400d361a06287dbfb034703417a672d83f5
shashiguru <30490543+shashiguru@users.noreply.github.com>
2022-02-22 15:21:21 +0530

    Add files via upload

---
+++ TechnicalChallegeValueLabs/ClassMarks/Models/Marks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMarks.Models
+{
+   public class SubjectMarks
+    {
+        public double Math1 { get; set; }
+        public double His { get; set; }
+        public double Geo { get; set; }
+        public double Eng { get; set; }
+    }
+
+    public class ClassesMarks
+    {
+
+        public SubjectMarks Class7 { get; set; }
+        public SubjectMarks Class8 { get; set; }
+        public SubjectMarks Class9 { get; set; }
+        public SubjectMarks Class10 { get; set; }
+        public SubjectMarks ClassN { get; set; }
+    }
+}
